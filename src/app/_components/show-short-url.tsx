6dca3b8cfb37a70aa85d89f306cde6ceb57b1365
Wayne <fssher107@gmail.com>
2023-10-18 22:37:20 +0800

    feat: qrcode support

--- src/app/_components/show-short-url.tsx
+++ src/app/_components/show-short-url.tsx
@@ -1,3 +1,5 @@
+import QRCode from "react-qr-code";
+
 import { useState } from "react";
 import { Button } from "./ui/button";
 import {
@@ -22,20 +24,28 @@ export default function ShowShortUrl({
         <CardTitle>Shortened Url is ready! 🥳</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="grid w-full items-center gap-2">
-          <p className="scroll-m-20 text-xl font-semibold tracking-tight">
-            New url:
-          </p>
-          <a href={url}> {url} </a>
-          <p className="scroll-m-20 text-xl font-semibold tracking-tight">
-            Old url:
-          </p>
-          <p>{originalUrl}</p>
+        <div className="row flex">
+          <div className="grid w-full items-center gap-2">
+            <p className="scroll-m-20 text-xl font-semibold tracking-tight">
+              New url:
+            </p>
+            <a href={url}> {url} </a>
+            <p className="scroll-m-20 text-xl font-semibold tracking-tight">
+              Old url:
+            </p>
+            <p>{originalUrl}</p>
+          </div>
+          <QRCode
+            size={256}
+            style={{ height: "auto", maxWidth: "50%", width: "50%" }}
+            value={url}
+            viewBox={`0 0 256 256`}
+          />
         </div>
       </CardContent>
       <CardFooter>
         <Button
-          onClick={async() => {
+          onClick={async () => {
             await navigator.clipboard.writeText(url);
             setIsCopied(true);
           }}
