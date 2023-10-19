28c67e92a8af9e426f1a0e722b85145fe6b7561d
Wayne <fssher107@gmail.com>
2023-10-19 23:03:10 +0800

    hotfix: temporary disable clerk auth for system accessability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { headers } from "next/headers";
@@ -24,19 +23,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <Head>
-          <title>{metadata.title}</title>
-          <meta name="description" content={metadata.description} />
-          {metadata.icons.map((icon, index) => (
-            <link key={index} rel={icon.rel} href={icon.url} />
-          ))}
-        </Head>
-        <body className={`font-sans ${inter.variable}`}>
-          <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {metadata.icons.map((icon, index) => (
+          <link key={index} rel={icon.rel} href={icon.url} />
+        ))}
+      </Head>
+      <body className={`font-sans ${inter.variable}`}>
+        <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
+      </body>
+    </html>
   );
 }
