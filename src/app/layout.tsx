f67855ed452f77d3a6e71e335f38f5a911770902
Wayne <fssher107@gmail.com>
2023-10-19 23:10:36 +0800

    feat: oauth w/ Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { headers } from "next/headers";
@@ -23,17 +24,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {metadata.icons.map((icon, index) => (
-          <link key={index} rel={icon.rel} href={icon.url} />
-        ))}
-      </Head>
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <Head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+          {metadata.icons.map((icon, index) => (
+            <link key={index} rel={icon.rel} href={icon.url} />
+          ))}
+        </Head>
+        <body className={`font-sans ${inter.variable}`}>
+          <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
