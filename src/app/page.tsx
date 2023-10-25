da5a4012a70833ac88306ff3bf70f2ade92e3097
Wayne <fssher107@gmail.com>
2023-10-25 15:48:01 +0800

    feat: show project roadmap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
 import { CreateShortUrl } from "~/app/_components/create-short-url";
-import { Github } from "lucide-react";
+import { Github, ListTodo } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <header className="absolute right-0 top-0 p-4">
-        <Link href={process.env.NEXT_PUBLIC_GITHUB_REPO_URL!}>
-          <Github className="h-8 w-8" />
-        </Link>
+        <div className="flex flex-row gap-4">
+          <Link href="https://zip-url.vercel.app/roadmap">
+            <ListTodo className="h-8 w-8" />
+          </Link>
+          <Link href={process.env.NEXT_PUBLIC_GITHUB_REPO_URL!}>
+            <Github className="h-8 w-8" />
+          </Link>
+        </div>
       </header>
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
