80121374774abc5fa4e6fd1d6c0c0724a11a7b7d
Wayne <fssher107@gmail.com>
2023-11-08 14:18:41 +0800

    feat: dashboard view

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,22 +4,44 @@ import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
 import { SignInOutButton } from "./authentication-button";
 import { Github, ListTodo } from "lucide-react";
+import { Home } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
   const { isLoaded: userLoaded, isSignedIn, user } = useUser();
   // Return empty div if user isn't loaded
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+
   if (!userLoaded) return <div />;
 
   return (
-    <header className="absolute right-0 top-0 p-4">
-      <div className="flex flex-row gap-4">
-        <SignInOutButton isSignedIn={isSignedIn} user={user} />
-        <Link href="https://zip-url.vercel.app/roadmap">
-          <ListTodo className="h-8 w-8" />
-        </Link>
-        <Link href={process.env.NEXT_PUBLIC_GITHUB_REPO_URL!}>
-          <Github className="h-8 w-8" />
-        </Link>
+    <header>
+      {!isHome && (
+        <div className="absolute left-0 top-0 p-4">
+          <Link href={window.location.origin}>
+            <Home className="h-8 w-8" />
+          </Link>
+        </div>
+      )}
+      <div className="absolute right-0 top-0 p-4">
+        <div className="flex flex-row gap-4">
+          <SignInOutButton isSignedIn={isSignedIn} user={user} />
+          {isSignedIn && (
+            <Link
+              href={window.location.origin + "/dashboard"}
+              style={{ marginTop: "4px" }}
+            >
+              Dashboard
+            </Link>
+          )}
+          <Link href="https://zip-url.vercel.app/roadmap">
+            <ListTodo className="h-8 w-8" />
+          </Link>
+          <Link href={process.env.NEXT_PUBLIC_GITHUB_REPO_URL!}>
+            <Github className="h-8 w-8" />
+          </Link>
+        </div>
       </div>
     </header>
   );
