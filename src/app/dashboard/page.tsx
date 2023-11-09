06f31a48815e239def7093610de3d6bbd8a37284
Wayne <fssher107@gmail.com>
2023-11-09 13:27:41 +0800

    style: change title grammer

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
       <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-        Shorten Your <span className="text-[hsl(280,100%,70%)]">URL</span>
+        Your Shortened <span className="text-[hsl(280,100%,70%)]">URL</span>
       </h1>
       <div className="rounded-xl bg-white px-5 py-5 text-black">
         <UrlTable urlRecords={urlRecords} currentUrl={window.location.origin} />
